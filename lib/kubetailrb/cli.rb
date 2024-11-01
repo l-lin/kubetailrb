6a2fc0ffa21c11c38d2da11dd5484f9a4dc64502
Louis Lin <lin.louis@pm.me>
2024-11-01 16:34:38 +0100

    refactor: check duck typing

--- lib/kubetailrb/cli.rb
+++ lib/kubetailrb/cli.rb
@@ -10,13 +10,12 @@ module Kubetailrb
   class CLI
     def execute(*args)
       cmd = OptsParser.new(*args).parse
+
       # NOTE: Is it better to use this approach by checking the method existence
       # or is it better to use a raise/rescue approach? Or another approach?
-      if cmd.respond_to?(:execute)
-        cmd.execute
-      else
-        puts 'invalid cmd'
-      end
+      raise 'Invalid cmd' unless cmd.respond_to?(:execute)
+
+      cmd.execute
 
       # TODO: Implement graceful shutdown in case of error.
     end
